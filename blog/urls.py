6bf1c3d71dddc78cc0b2af32de986edec42a11d2
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2024-10-25 10:19:47 -0300

    Commit parte 2

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('post/<int:post_id>', views.post, name='post'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
 ]
