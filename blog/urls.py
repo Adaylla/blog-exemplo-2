82a68fd203eb62d6e87e2b7fd311089fd1a1ff84
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2024-11-22 10:26:22 -0300

    não está quase lá... :( crud

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
     path('mensagem/', views.mensagem, name='mensagem'),
+    path('mensagem/<int:mensagem_id>/editar', views.editar_mensagem, name='editar_mensagem'),
+    path('mensagem/<int:mensagem_id>/deletar', views.deletar_mensagem, name='deletar_mensagem'),
 ]
